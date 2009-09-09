f98ae68c920c887521e97cf8478c5c14f0c323d9
uwe <uwe@2b7e53f0-3cfb-0310-b3e9-8179ed1497e1>
2009-09-09 00:55:13 +0000

    Replace pseudonym in drkaiser.c with real name.
    Signed-off-by: Joerg Fischer <turboj@gmx.de>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://flashrom.org/flashrom/trunk@720 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- drkaiser.c
+++ drkaiser.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the flashrom project.
  *
- * Copyright (C) 2009 TURBO J <turboj@web.de>
+ * Copyright (C) 2009 Joerg Fischer <turboj@web.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
