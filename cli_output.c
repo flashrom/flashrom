aae9c298d15e53957be4525c6ddff03b96468bd8
snelson <snelson@2b7e53f0-3cfb-0310-b3e9-8179ed1497e1>
2010-01-07 20:21:58 +0000

    Converting fprintf(stderr), printf, and printf_debug into a common print interface for flashrom. It also changes so -VV will spit out highly verbose messages for debugging. This is a minimal patch to lessen impact a later patch will convert current printf messages to the new interface.
    Add file that was suppose to be committed with r835.
    
    Signed-off-by: Sean Nelson <audiohacked@gmail.com>
    Acked-by: Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>
    
    
    
    git-svn-id: svn://flashrom.org/flashrom/trunk@836 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ cli_output.c
@@ -0,0 +1,51 @@
+/*
+ * This file is part of the flashrom project.
+ *
+ * Copyright (C) 2009 Sean Nelson <audiohacked@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include "flash.h"
+
+int print(int type, const char *fmt, ...)
+{
+	va_list ap;
+	int ret;
+	FILE *output_type;
+	
+	switch (type)
+	{
+	case MSG_ERROR:
+		output_type = stderr;
+		break;
+	case MSG_BARF:
+		if (verbose < 2) return 0;
+	case MSG_DEBUG:
+		if (verbose < 1) return 0;
+	case MSG_INFO:
+	default:
+		output_type = stdout;
+		break;
+	}
+	
+	va_start(ap, fmt);
+	ret = vfprintf(output_type, fmt, ap);
+	va_end(ap);
+	return ret;
+}
+
