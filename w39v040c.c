45d8c2ff8a048f21518e3943f6c991957faf7ffd
snelson <snelson@2b7e53f0-3cfb-0310-b3e9-8179ed1497e1>
2010-03-16 00:35:53 +0000

    kill dead w39v040c.c functions Signed-off-by: Sean Nelson <audiohacked@gmail.com> Acked-by: Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>
    git-svn-id: svn://flashrom.org/flashrom/trunk@939 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- w39v040c.c
+++ w39v040c.c
@@ -21,36 +21,6 @@
 #include "flash.h"
 #include "chipdrivers.h"
 
-int probe_w39v040c(struct flashchip *flash)
-{
-	chipaddr bios = flash->virtual_memory;
-	int result = probe_jedec(flash);
-	uint8_t lock;
-
-	if (!result)
-		return result;
-
-	chip_writeb(0xAA, bios + 0x5555);
-	programmer_delay(10);
-	chip_writeb(0x55, bios + 0x2AAA);
-	programmer_delay(10);
-	chip_writeb(0x90, bios + 0x5555);
-	programmer_delay(10);
-
-	lock = chip_readb(bios + 0xfff2);
-
-	chip_writeb(0xAA, bios + 0x5555);
-	programmer_delay(10);
-	chip_writeb(0x55, bios + 0x2AAA);
-	programmer_delay(10);
-	chip_writeb(0xF0, bios + 0x5555);
-	programmer_delay(40);
-
-	printf("%s: Boot block #TBL is %slocked, rest of chip #WP is %slocked.\n",
-		__func__, lock & 0x4 ? "" : "un", lock & 0x8 ? "" : "un");
-	return 1;
-}
-
 int printlock_w39v040c(struct flashchip *flash)
 {
 	chipaddr bios = flash->virtual_memory;
@@ -74,44 +44,5 @@ int printlock_w39v040c(struct flashchip *flash)
 
 	printf("%s: Boot block #TBL is %slocked, rest of chip #WP is %slocked.\n",
 		__func__, lock & 0x4 ? "" : "un", lock & 0x8 ? "" : "un");
-	return 0;
-}
-
-int erase_w39v040c(struct flashchip *flash)
-{
-	int i;
-	unsigned int total_size = flash->total_size * 1024;
-
-	for (i = 0; i < total_size; i += flash->page_size) {
-		if (erase_sector_jedec(flash, i, flash->page_size)) {
-			fprintf(stderr, "ERASE FAILED!\n");
-			return -1;
-		}
-	}
-
-	return 0;
-}
-
-int write_w39v040c(struct flashchip *flash, uint8_t *buf)
-{
-	int i;
-	int total_size = flash->total_size * 1024;
-	int page_size = flash->page_size;
-	chipaddr bios = flash->virtual_memory;
-
-	if (erase_flash(flash)) {
-		fprintf(stderr, "ERASE FAILED!\n");
-		return -1;
-	}
-
-	printf("Programming page: ");
-	for (i = 0; i < total_size / page_size; i++) {
-		printf("%04d at address: 0x%08x", i, i * page_size);
-		write_sector_jedec_common(flash, buf + i * page_size,
-				   bios + i * page_size, page_size, 0xffff);
-		printf("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
-	}
-	printf("\n");
-
 	return 0;
 }
