4f782dea24b01d52d2185de18148428123639dd0
stuge <stuge@2b7e53f0-3cfb-0310-b3e9-8179ed1497e1>
2010-06-17 00:01:56 +0000

    Set maximum flash size for 3Com NICs to 128kb = 1Mbit
    This fact was already documented on http://flashrom.org/NIC3Com
    
    Signed-off-by: Peter Stuge <peter@stuge.se>
    Acked-by: Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>
    
    
    git-svn-id: svn://flashrom.org/flashrom/trunk@1049 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- nic3com.c
+++ nic3com.c
@@ -81,6 +81,7 @@ int nic3com_init(void)
 	OUTW(SELECT_REG_WINDOW + 0, io_base_addr + INT_STATUS);
 
 	buses_supported = CHIP_BUSTYPE_PARALLEL;
+	max_rom_decode.parallel = 128 * 1024;
 
 	return 0;
 }
