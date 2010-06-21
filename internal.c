06ba1db11dbfdd0c295e6b0f6e14cfb624e7727a
hailfinger <hailfinger@2b7e53f0-3cfb-0310-b3e9-8179ed1497e1>
2010-06-21 23:20:15 +0000

    Kill unneeded #include wherever possible.
    Tested on Linux, FreeBSD, NetBSD, OpenBSD, DOS.
    Thanks to Jonathan A. Kollasch and Idwer Vollering for testing.
    
    Signed-off-by: Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>
    Acked-by: Idwer Vollering <vidwer+lists.flashrom@gmail.com>
    
    
    git-svn-id: svn://flashrom.org/flashrom/trunk@1057 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- internal.c
+++ internal.c
@@ -21,10 +21,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <errno.h>
 #include "flash.h"
 
 #if NEED_PCI == 1
