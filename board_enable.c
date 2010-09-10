4a23e44bea0efb2b3d91ddb8b1a3bb4c3be66dcb
Michael Karcher <flashrom@mkarcher.dialup.fu-berlin.de>
2010-09-10 14:46:46 +0000

    Board enable for ASUS P5GD1 Pro
    lspci/superiotool:
      http://www.coreboot.org/pipermail/flashrom/2010-August/004539.html
    
    matching SMBus + Audio, because SMBus is the only core device with
    usable IDs.
    
    Corresponding to flashrom svn r1156.
    
    Signed-off-by: Michael Karcher <flashrom@mkarcher.dialup.fu-berlin.de>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>

--- board_enable.c
+++ board_enable.c
@@ -1348,6 +1348,7 @@ static int intel_ich_gpio19_raise(void)
  *  - ASUS P4B266LM (Sony Vaio PCV-RX650): socket478 + 845D + ICH2
  *  - ASUS P4C800-E Deluxe: socket478 + 875P + ICH5
  *  - ASUS P4P800-E Deluxe: Intel socket478 + 865PE + ICH5R
+ *  - ASUS P5GD1 Pro: Intel LGA 775 + 915P + ICH6R
  *  - ASUS P5PE-VM: Intel LGA775 + 865G + ICH5
  *  - Samsung Polaris 32: socket478 + 865P + ICH5
  */
@@ -1807,6 +1808,7 @@ const struct board_pciid_enable board_pciid_enables[] = {
 	{0x8086, 0x2570, 0x1043, 0x80A5,  0x105A, 0x24D3, 0x1043, 0x80A6, NULL,          NULL,         NULL,          "ASUS",        "P4SD-LA",               0,   NT, intel_ich_gpio32_raise},
 	{0x1039, 0x0661, 0x1043, 0x8113,  0x1039, 0x5513, 0x1043, 0x8087, NULL,          NULL,         NULL,          "ASUS",        "P4S800-MX",             512, OK, w836xx_memw_enable_2e},
 	{0x10B9, 0x1541,      0,      0,  0x10B9, 0x1533,      0,      0, "^P5A$",       "asus",       "p5a",         "ASUS",        "P5A",                   0,   OK, board_asus_p5a},
+	{0x8086, 0x266a, 0x1043, 0x80a6,  0x8086, 0x2668, 0x1043, 0x814e, NULL,          NULL,         NULL,          "ASUS",        "P5GD1 Pro",             0,   OK, intel_ich_gpio21_raise},
 	{0x10DE, 0x0030, 0x1043, 0x818a,  0x8086, 0x100E, 0x1043, 0x80EE, NULL,          NULL,         NULL,          "ASUS",        "P5ND2-SLI Deluxe",      0,   OK, nvidia_mcp_gpio10_raise},
 	{0x8086, 0x24dd, 0x1043, 0x80a6,  0x8086, 0x2570, 0x1043, 0x8157, NULL,          NULL,         NULL,          "ASUS",        "P5PE-VM",               0,   OK, intel_ich_gpio21_raise},
 	{0x8086, 0x3590, 0x1028, 0x016c,  0x1000, 0x0030, 0x1028, 0x016c, NULL,          NULL,         NULL,          "Dell",        "PowerEdge 1850",        0,   OK, intel_ich_gpio23_raise},
