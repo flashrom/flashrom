39dcdece3ad5d629b80958df8f193f0763e878d3
Michael Karcher <flashrom@mkarcher.dialup.fu-berlin.de>
2010-10-05 17:29:35 +0000

    Board enable for GA-6IEM
    Reported by Konstantin <hc@comp.susu.ac.ru>
    lspci (superiotool missing, doesn't matter for this patch)
     http://www.coreboot.org/pipermail/flashrom/2010-September/004609.html
    DMI is needed, as there are no usefull PCI IDs.
    
    (no test of that board yet, thus marked as untested)
    
    Corresponding to flashrom svn r1187.
    
    Signed-off-by: Michael Karcher <flashrom@mkarcher.dialup.fu-berlin.de>
    Acked-by: Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>

--- board_enable.c
+++ board_enable.c
@@ -1482,6 +1482,7 @@ static int intel_ich_gpio23_raise(void)
 
 /*
  * Suited for:
+ *  - GIGABYTE GA-6IEM: Intel Socket370 + i815 + ICH2
  *  - GIGABYTE GA-8IRML: Intel Socket478 + i845 + ICH2
  */
 static int intel_ich_gpio25_raise(void)
@@ -1909,6 +1910,7 @@ const struct board_pciid_enable board_pciid_enables[] = {
 	{0x1106, 0x3177, 0x1106, 0x3177,  0x1106, 0x3059, 0x1695, 0x3005, NULL,          NULL,         NULL,          "EPoX",        "EP-8K5A2",              0,   OK, w836xx_memw_enable_2e},
 	{0x10EC, 0x8139, 0x1695, 0x9001,  0x11C1, 0x5811, 0x1695, 0x9015, NULL,          NULL,         NULL,          "EPoX",        "EP-8RDA3+",             0,   OK, nvidia_mcp_gpio31_raise},
 	{0x8086, 0x7110,      0,      0,  0x8086, 0x7190,      0,      0, NULL,          "epox",       "ep-bx3",      "EPoX",        "EP-BX3",                0,   NT, intel_piix4_gpo22_raise},
+	{0x8086, 0x2443, 0x8086, 0x2442,  0x8086, 0x1130, 0x8086, 0x1130, "^6IEM ",      NULL,         NULL,          "GIGABYTE",    "GA-6IEM",               0,   NT, intel_ich_gpio25_raise},
 	{0x1106, 0x0686, 0x1106, 0x0686,  0x1106, 0x3058, 0x1458, 0xa000, NULL,          NULL,         NULL,          "GIGABYTE",    "GA-7ZM",                512, OK, NULL},
 	{0x8086, 0x244b, 0x8086, 0x2442,  0x8086, 0x2445, 0x1458, 0xa002, NULL,          NULL,         NULL,          "GIGABYTE",    "GA-8IRML",              0,   OK, intel_ich_gpio25_raise},
 	{0x8086, 0x24c3, 0x1458, 0x24c2,  0x8086, 0x24cd, 0x1458, 0x5004, NULL,          NULL,         NULL,          "GIGABYTE",    "GA-8PE667 Ultra 2",     0,   OK, intel_ich_gpio32_raise},
