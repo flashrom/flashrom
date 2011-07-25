716e09848a85d1b79dd1a4a6ec20707febeee2f0
Stefan Tauner <stefan.tauner@alumni.tuwien.ac.at>
2011-07-25 20:38:52 +0000

    Add a bunch of new/tested stuff and various small changes 5
    - mark EN25F80 as fully tested
      http://www.flashrom.org/pipermail/flashrom/2011-July/007329.html
    - mark W25Q16 as fully tested
      http://www.flashrom.org/pipermail/flashrom/2011-July/007151.html
    - mark W39V040A as fully tested
      http://www.flashrom.org/pipermail/flashrom/2011-July/007161.html
    - mark Pm25LV040 as fully tested
      reported by TL1 on IRC
    - mark W49F002U/N as fully tested
      http://paste.flashrom.org/view.php?id=733g
    - mark W39V080FA as fully tested
      http://www.flashrom.org/pipermail/flashrom/2011-July/007225.html
    
    - add ASUS P4S533-X to the list of supported boards
      http://www.flashrom.org/pipermail/flashrom/2011-July/007200.html
    - add ASUS M4A785TD-V EVO to the list of supported boards
      http://www.flashrom.org/pipermail/flashrom/2011-July/007329.html
    - add GA-945PL-S3P (rev. 6.6) to the list of supported boards
      reported by TL1 on IRC
    - add MS-7142 (K8MM-V) to the list of supported boards
      http://www.flashrom.org/pipermail/flashrom/2011-July/007161.html
    - add MS-7369 (K9N Neo V2) to the list of supported boards
      http://www.flashrom.org/pipermail/flashrom/2011-July/007181.html
    - add X7DBT-INF to the list of supported boards
      http://www.flashrom.org/pipermail/flashrom/2011-July/007225.html
    
    - mark SiS 645DX chipset enable as OK
      http://www.flashrom.org/pipermail/flashrom/2011-July/007200.html
    - mark SiS 651 chipset enable as OK
      http://paste.flashrom.org/view.php?id=733
    
    - move intel_ich_gpio34_raise to the correct line(s)
    - change the output of unlock_w39_fwh_block from 0x%x to 0x%08x
    - fix output for untested chipset enables (missing space)
    - reorder the board enable in print.c entry for GA-8SIMLH added in r1385.
    - minor other fixes
    
    - fix output for multiple found flash chips by adding quotes and commas
    - similarly fix output of "Found/Assuming" chips
    
    Corresponding to flashrom svn r1386.
    
    Signed-off-by: Stefan Tauner <stefan.tauner@alumni.tuwien.ac.at>
    the last two points are
    Acked-by: Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>
    everything else is
    Acked-by: Stefan Tauner <stefan.tauner@alumni.tuwien.ac.at>

--- flashrom.c
+++ flashrom.c
@@ -1201,7 +1201,7 @@ notfound:
 		snprintf(location, sizeof(location), "on %s", programmer_table[programmer].name);
 
 	tmp = flashbuses_to_text(flash->bustype);
-	msg_cinfo("%s chip \"%s %s\" (%d kB, %s) %s.\n",
+	msg_cinfo("%s %s flash chip \"%s\" (%d kB, %s) %s.\n",
 		  force ? "Assuming" : "Found", fill_flash->vendor,
 		  fill_flash->name, fill_flash->total_size, tmp, location);
 	free(tmp);
@@ -1711,7 +1711,7 @@ int selfcheck(void)
 		ret = 1;
 	}
 	/* It would be favorable if we could also check for correct terminaion
-	 * of the follwing arrays, but we don't know their size in here...
+	 * of the following arrays, but we don't know their sizes in here...
 	 * For 'flashchips' we check the first element to be non-null. In the
 	 * other cases there exist use cases where the first element can be
 	 * null. */
