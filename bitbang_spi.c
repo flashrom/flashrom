4b636a73e0ff9e6557e1f15c37123a4c9621dda6
stefanct <stefanct@2b7e53f0-3cfb-0310-b3e9-8179ed1497e1>
2013-01-15 17:37:48 +0000

    Fix duplicate 'const' declaration specifiers.
    Thanks to Idwer and clang for noticing these problems.
    
    Signed-off-by: Stefan Tauner <stefan.tauner@student.tuwien.ac.at>
    Signed-off-by: Idwer Vollering <vidwer@gmail.com>
    Acked-by: Stefan Tauner <stefan.tauner@student.tuwien.ac.at>
    
    git-svn-id: svn://coreboot.org/flashrom/trunk@1646 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- bitbang_spi.c
+++ bitbang_spi.c
@@ -26,33 +26,33 @@
 #include "spi.h"
 
 /* Note that CS# is active low, so val=0 means the chip is active. */
-static void bitbang_spi_set_cs(const const struct bitbang_spi_master *master, int val)
+static void bitbang_spi_set_cs(const struct bitbang_spi_master * const master, int val)
 {
 	master->set_cs(val);
 }
 
-static void bitbang_spi_set_sck(const const struct bitbang_spi_master *master, int val)
+static void bitbang_spi_set_sck(const struct bitbang_spi_master * const master, int val)
 {
 	master->set_sck(val);
 }
 
-static void bitbang_spi_set_mosi(const const struct bitbang_spi_master *master, int val)
+static void bitbang_spi_set_mosi(const struct bitbang_spi_master * const master, int val)
 {
 	master->set_mosi(val);
 }
 
-static int bitbang_spi_get_miso(const const struct bitbang_spi_master *master)
+static int bitbang_spi_get_miso(const struct bitbang_spi_master * const master)
 {
 	return master->get_miso();
 }
 
-static void bitbang_spi_request_bus(const const struct bitbang_spi_master *master)
+static void bitbang_spi_request_bus(const struct bitbang_spi_master * const master)
 {
 	if (master->request_bus)
 		master->request_bus();
 }
 
-static void bitbang_spi_release_bus(const const struct bitbang_spi_master *master)
+static void bitbang_spi_release_bus(const struct bitbang_spi_master * const master)
 {
 	if (master->release_bus)
 		master->release_bus();
