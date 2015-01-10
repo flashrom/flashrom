40bc96fca241970b7cd9632faa8dbbc9e1d617e8
Stefan Tauner <stefan.tauner@alumni.tuwien.ac.at>
2015-01-10 09:33:14 +0000

    Add support for Macronix MX25L6495F
    This is based on the code sent to the flashrom mailing list
    by Alex Lu (alexlu6@mxic.com.tw) without sign-off.
    
    Corresponding to flashrom svn r1867.
    
    Signed-off-by: Stefan Tauner <stefan.tauner@alumni.tuwien.ac.at>
    Acked-by: Stefan Tauner <stefan.tauner@alumni.tuwien.ac.at>

--- flashchips.c
+++ flashchips.c
@@ -7280,6 +7280,43 @@ const struct flashchip flashchips[] = {
 	},
 
 	{
+		.name		= "MX25L6495F",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= MACRONIX_ID,
+		.model_id	= MACRONIX_MX25L6495F,
+		.total_size	= 8192,
+		.page_size	= 256,
+		/* OTP: 1024B total; enter 0xB1, exit 0xC1 */
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP,
+		.tested		= TEST_OK_PREW,
+		.probe		= probe_spi_rdid,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 2048} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {64 * 1024, 128} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {32 * 1024, 256} },
+				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {8 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_60,
+			}, {
+				.eraseblocks = { {8 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.unlock		= spi_disable_blockprotect,
+		.write		= spi_chip_write_256,
+		.read		= spi_chip_read, /* Fast read (0x0B) and multi I/O supported */
+		.voltage	= {2700, 3600},
+	},
+
+	{
 		.vendor		= "Macronix",
 		.name		= "MX29F001B",
 		.bustype	= BUS_PARALLEL,
