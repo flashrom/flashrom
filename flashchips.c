70eed9ff60af62604a856940d5b126909e2d679e
Nico Huber <nico.h@gmx.de>
2017-04-24 22:19:27 +0200

    flashchips: Add untested Winbond W25Q128.W
    Only difference to its sibling W25Q128.V seems to be the supply voltage.
    
    Change-Id: I34ce7f1bdd0d2fb1b065031e5a689bb16ffc70db
    Signed-off-by: Nico Huber <nico.h@gmx.de>
    Reviewed-on: https://review.coreboot.org/19436
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: David Hendricks <david.hendricks@gmail.com>

--- flashchips.c
+++ flashchips.c
@@ -14825,6 +14825,46 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "Winbond",
+		.name		= "W25Q128.W",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= WINBOND_NEX_ID,
+		.model_id	= WINBOND_NEX_W25Q128_W,
+		.total_size	= 16384,
+		.page_size	= 256,
+		/* supports SFDP */
+		/* OTP: 1024B total, 256B reserved; read 0x48; write 0x42, erase 0x44, read ID 0x4B */
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_QPI,
+		.tested		= TEST_UNTESTED,
+		.probe		= probe_spi_rdid,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 4096} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 512} },
+				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 256} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {16 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_60,
+			}, {
+				.eraseblocks = { {16 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.printlock	= spi_prettyprint_status_register_plain, /* TODO: improve */
+		.unlock		= spi_disable_blockprotect,
+		.write		= spi_chip_write_256,
+		.read		= spi_chip_read,
+		.voltage	= {1650, 1950},
+	},
+
+	{
+		.vendor		= "Winbond",
 		.name		= "W25X10",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= WINBOND_NEX_ID,
