c9ee0ed8a62d5b165a22d536753e960e0158460c
David Hendricks <david.hendricks@gmail.com>
2018-02-11 17:40:53 -0800

    flashchips: Add Winbond W25P80/16/32 support
    This adds support for W25P80/16/32 chips. Most notably these chips only
    have two erase commands - one for 64KiB "sectors" and one for chip
    erase.
    
    Change-Id: Ie09ba8e28fee35c42e17ca05219dc673413de93b
    Signed-off-by: David Hendricks <david.hendricks@gmail.com>
    Reviewed-on: https://review.coreboot.org/23700
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- flashchips.c
+++ flashchips.c
@@ -15430,6 +15430,92 @@ const struct flashchip flashchips[] = {
 	},
 
 	{
+		.vendor         = "Winbond",
+		.name           = "W25P80",
+		.bustype        = BUS_SPI,
+		.manufacture_id = WINBOND_NEX_ID,
+		.model_id       = WINBOND_NEX_W25P80,
+		.total_size     = 1024,
+		.page_size      = 256,
+		.feature_bits   = FEATURE_WRSR_WREN,
+		.tested         = TEST_UNTESTED,
+		.probe          = probe_spi_rdid,
+		.probe_timing   = TIMING_ZERO,
+		.block_erasers  =
+		{
+			{
+				.eraseblocks = { {64 * 1024, 16} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {1024 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.printlock      = spi_prettyprint_status_register_plain, /* TODO: improve */
+		.unlock         = spi_disable_blockprotect,
+		.write          = spi_chip_write_256,
+		.read           = spi_chip_read, /* Fast read (0x0B) supported */
+		.voltage        = {2700, 3600},
+	},
+
+	{
+		.vendor         = "Winbond",
+		.name           = "W25P16",
+		.bustype        = BUS_SPI,
+		.manufacture_id = WINBOND_NEX_ID,
+		.model_id       = WINBOND_NEX_W25P16,
+		.total_size     = 2048,
+		.page_size      = 256,
+		.feature_bits   = FEATURE_WRSR_WREN,
+		.tested         = TEST_UNTESTED,
+		.probe          = probe_spi_rdid,
+		.probe_timing   = TIMING_ZERO,
+		.block_erasers  =
+		{
+			{
+				.eraseblocks = { {64 * 1024, 32} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {2048 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.printlock      = spi_prettyprint_status_register_plain, /* TODO: improve */
+		.unlock         = spi_disable_blockprotect,
+		.write          = spi_chip_write_256,
+		.read           = spi_chip_read, /* Fast read (0x0B) supported */
+		.voltage        = {2700, 3600},
+	},
+
+	{
+		.vendor         = "Winbond",
+		.name           = "W25P32",
+		.bustype        = BUS_SPI,
+		.manufacture_id = WINBOND_NEX_ID,
+		.model_id       = WINBOND_NEX_W25P32,
+		.total_size     = 4096,
+		.page_size      = 256,
+		.feature_bits   = FEATURE_WRSR_WREN,
+		.tested         = TEST_UNTESTED,
+		.probe          = probe_spi_rdid,
+		.probe_timing   = TIMING_ZERO,
+		.block_erasers  =
+		{
+			{
+				.eraseblocks = { {64 * 1024, 64} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {4096 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.printlock      = spi_prettyprint_status_register_plain, /* TODO: improve */
+		.unlock         = spi_disable_blockprotect,
+		.write          = spi_chip_write_256,
+		.read           = spi_chip_read, /* Fast read (0x0B) supported */
+		.voltage        = {2700, 3600},
+	},
+	{
 		.vendor		= "Winbond",
 		.name		= "W29C512A/W29EE512",
 		.bustype	= BUS_PARALLEL,
