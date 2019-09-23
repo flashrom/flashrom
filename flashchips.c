6f6b98a05a0c92057f3e5c77b69811a2f7da7541
Alan Green <avg@google.com>
2019-09-23 17:12:44 +1000

    flashchips.c: Add W25Q128.V..M printlock attribute
    Add a printlock attribute for the Winbond W25Q128.V..M chip. The
    printlock attributes matches the ChromiumOS repo's definition of this
    chip.
    
    Signed-off-by: Alan Green <avg@google.com>
    Change-Id: I775d5d40677593dcb2d05750f8bbc62871b0e551
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/35549
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>

--- flashchips.c
+++ flashchips.c
@@ -15844,6 +15844,7 @@ const struct flashchip flashchips[] = {
 				.block_erase = spi_block_erase_c7,
 			}
 		},
+		.printlock	= spi_prettyprint_status_register_plain,
 		.unlock		= spi_disable_blockprotect,
 		.write		= spi_chip_write_256,
 		.read		= spi_chip_read,
