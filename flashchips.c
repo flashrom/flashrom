4139438943010c7aec6549d8b18865da5b70e978
darkarnium <peter.adkins@kernelpicnic.net>
2019-11-04 20:06:48 +0000

    flashchips: Add AT25SF321
    This commit adds support for the Adesto AT25SF321 SPI flash chip. Probe
    and read operations have been tested via FT2232H interface, but writes
    have not been verified.
    
    Datasheet is available at the following URL:
    https://www.adestotech.com/wp-content/uploads/DS-AT25SF321_047.pdf
    
    Change-Id: I7410815e063ffe154a97d7ea5881c8eb82025f56
    Signed-off-by: Peter Adkins <pete@kernelpicnic.net>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/36904
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: David Hendricks <david.hendricks@gmail.com>

--- flashchips.c
+++ flashchips.c
@@ -2331,6 +2331,44 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "Atmel",
+		.name		= "AT25SF321",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= ATMEL_ID,
+		.model_id	= ATMEL_AT25SF321,
+		.total_size	= 4096,
+		.page_size	= 256,
+		.feature_bits	= FEATURE_WRSR_WREN,
+		.tested		= TEST_OK_PR,
+		.probe		= probe_spi_rdid,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 1024} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 128} },
+				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 64} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {4096 * 1024, 1} },
+				.block_erase = spi_block_erase_60,
+			}, {
+				.eraseblocks = { {4096 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.printlock	= spi_prettyprint_status_register_plain,
+		.unlock		= spi_disable_blockprotect,
+		.write		= spi_chip_write_256,
+		.read		= spi_chip_read,
+		.voltage	= {2500, 3600},
+	},
+
+	{
+		.vendor		= "Atmel",
 		.name		= "AT25SL128A",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= ATMEL_ID,
