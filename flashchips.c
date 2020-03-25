2e286074b47f378380e87e673b44030f3050c568
Dino Li <Dino.Li@ite.com.tw>
2020-03-25 17:39:53 +0800

    flashchips: add support for GigaDevice GD25WQ80E
    Support GD25WQ80E, which is the internal flash of IT81202.
    
    TEST=Building flashrom and flashing FW image into IT81202 successfully.
    
    Change-Id: Ib5feaa6ecc7b11b2218e5f02c087b4331388bef8
    Signed-off-by: Dino Li <Dino.Li@ite.com.tw>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/39820
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>

--- flashchips.c
+++ flashchips.c
@@ -6787,6 +6787,44 @@ const struct flashchip flashchips[] = {
 	},
 
 	{
+		.vendor		= "GigaDevice",
+		.name		= "GD25WQ80E",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= GIGADEVICE_ID,
+		.model_id	= GIGADEVICE_GD25WQ80E,
+		.total_size	= 1024,
+		.page_size	= 256,
+		.feature_bits	= FEATURE_WRSR_WREN,
+		.tested		= TEST_OK_PREW,
+		.probe		= probe_spi_rdid,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 256} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 32} },
+				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 16} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {1 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_60,
+			}, {
+				.eraseblocks = { {1 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.printlock	= spi_prettyprint_status_register_bp4_srwd,
+		.unlock		= spi_disable_blockprotect_bp4_srwd,
+		.write		= spi_chip_write_256,
+		.read		= spi_chip_read,
+		.voltage	= {1650, 3600},
+	},
+
+	{
 		.vendor		= "Hyundai",
 		.name		= "HY29F002B",
 		.bustype	= BUS_PARALLEL,
