cb8d343a05f849f81f1db22be29e417a856bad86
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-08-26 09:15:53 +0200

    flashchips: Fix W25Q256.W
    The JW is the only known variant. A W25Q256FW may have existed with
    less 4BA instructions supported, but it never showed up and no data-
    sheet is available.
    
    Used the datasheet from here:
    https://www.winbond.com/resource-files/w25q256jw%20spi%20revb%2012082017.pdf
    
    Change-Id: I9a3995c66ad7b74823e17984bf1ffac50b5663e0
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Signed-off-by: Nico Huber <nico.h@gmx.de>
    Ticket: https://ticket.coreboot.org/issues/362
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/44810
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- flashchips.c
+++ flashchips.c
@@ -17597,7 +17597,7 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "Winbond",
-		.name		= "W25Q256.W",
+		.name		= "W25Q256JW",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= WINBOND_NEX_ID,
 		.model_id	= WINBOND_NEX_W25Q256_W,
@@ -17605,8 +17605,7 @@ const struct flashchip flashchips[] = {
 		.page_size	= 256,
 		/* supports SFDP */
 		/* OTP: 1024B total, 256B reserved; read 0x48; write 0x42, erase 0x44, read ID 0x4B */
-		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_4BA_ENTER_WREN
-			| FEATURE_4BA_EXT_ADDR | FEATURE_4BA_READ | FEATURE_4BA_FAST_READ,
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_4BA,
 		.tested		= TEST_OK_PREW,
 		.probe		= probe_spi_rdid,
 		.probe_timing	= TIMING_ZERO,
@@ -17614,10 +17613,16 @@ const struct flashchip flashchips[] = {
 		{
 			{
 				.eraseblocks = { {4 * 1024, 8192} },
+				.block_erase = spi_block_erase_21,
+			}, {
+				.eraseblocks = { {4 * 1024, 8192} },
 				.block_erase = spi_block_erase_20,
 			}, {
 				.eraseblocks = { {32 * 1024, 1024} },
 				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 512} },
+				.block_erase = spi_block_erase_dc,
 			}, {
 				.eraseblocks = { {64 * 1024, 512} },
 				.block_erase = spi_block_erase_d8,
