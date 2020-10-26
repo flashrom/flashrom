d7b48758a5b2c9ab911b77370726071b30ac8c2c
Edward O'Callaghan <quasisec@google.com>
2020-10-26 19:05:32 +1100

    internal.c: Reshuffle functions to avoid forward decls
    This just makes internal.c a little easier to parse and avoids
    some fn prototypes on the mental stack.
    
    BUG=none
    BRANCH=none
    TEST=builds
    
    Change-Id: I693e30068e6a53b5fc161d895af451540650a8fe
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/46813
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- internal.c
+++ internal.c
@@ -114,19 +114,48 @@ int is_laptop = 0;
 int laptop_ok = 0;
 
 static void internal_chip_writeb(const struct flashctx *flash, uint8_t val,
-				 chipaddr addr);
+				 chipaddr addr)
+{
+	mmio_writeb(val, (void *) addr);
+}
+
 static void internal_chip_writew(const struct flashctx *flash, uint16_t val,
-				 chipaddr addr);
+				 chipaddr addr)
+{
+	mmio_writew(val, (void *) addr);
+}
+
 static void internal_chip_writel(const struct flashctx *flash, uint32_t val,
-				 chipaddr addr);
+				 chipaddr addr)
+{
+	mmio_writel(val, (void *) addr);
+}
+
 static uint8_t internal_chip_readb(const struct flashctx *flash,
-				   const chipaddr addr);
+				   const chipaddr addr)
+{
+	return mmio_readb((void *) addr);
+}
+
 static uint16_t internal_chip_readw(const struct flashctx *flash,
-				    const chipaddr addr);
+				    const chipaddr addr)
+{
+	return mmio_readw((void *) addr);
+}
+
 static uint32_t internal_chip_readl(const struct flashctx *flash,
-				    const chipaddr addr);
+				    const chipaddr addr)
+{
+	return mmio_readl((void *) addr);
+}
+
 static void internal_chip_readn(const struct flashctx *flash, uint8_t *buf,
-				const chipaddr addr, size_t len);
+				const chipaddr addr, size_t len)
+{
+	mmio_readn((void *)addr, buf, len);
+	return;
+}
+
 static const struct par_master par_master_internal = {
 		.chip_readb		= internal_chip_readb,
 		.chip_readw		= internal_chip_readw,
@@ -343,47 +372,4 @@ internal_init_exit:
 	free(board_model);
 
 	return ret;
-}
-
-static void internal_chip_writeb(const struct flashctx *flash, uint8_t val,
-				 chipaddr addr)
-{
-	mmio_writeb(val, (void *) addr);
-}
-
-static void internal_chip_writew(const struct flashctx *flash, uint16_t val,
-				 chipaddr addr)
-{
-	mmio_writew(val, (void *) addr);
-}
-
-static void internal_chip_writel(const struct flashctx *flash, uint32_t val,
-				 chipaddr addr)
-{
-	mmio_writel(val, (void *) addr);
-}
-
-static uint8_t internal_chip_readb(const struct flashctx *flash,
-				   const chipaddr addr)
-{
-	return mmio_readb((void *) addr);
-}
-
-static uint16_t internal_chip_readw(const struct flashctx *flash,
-				    const chipaddr addr)
-{
-	return mmio_readw((void *) addr);
-}
-
-static uint32_t internal_chip_readl(const struct flashctx *flash,
-				    const chipaddr addr)
-{
-	return mmio_readl((void *) addr);
-}
-
-static void internal_chip_readn(const struct flashctx *flash, uint8_t *buf,
-				const chipaddr addr, size_t len)
-{
-	mmio_readn((void *)addr, buf, len);
-	return;
 }
