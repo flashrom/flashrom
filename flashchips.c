7c63629f46e0de4d0be8af40c622d3ba397d54e9
Roman Stingler <roman.stingler@gmail.com>
2021-03-29 15:08:14 +0200

    Support For Macronix MX25V8035F
    Signed-off-by: Roman Stingler  <roman.stingler@gmail.com>
    
    closes #179
    
    Change-Id: Icaf19910703c5322054c92dda9d27d34c6678526

--- flashchips.c
+++ flashchips.c
@@ -9538,6 +9538,48 @@ const struct flashchip flashchips[] = {
 		.voltage	= {1650, 2000},
 	},
 
+
+		{
+		.vendor		= "Macronix",
+		.name		= "MX25V8035F",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= MACRONIX_ID,
+		.model_id	= MACRONIX_MX25V8035F,
+		.total_size	= 1024,
+		.page_size	= 256,
+		/* OTP: 512B total; enter 0xB1, exit 0xC1 */
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP,
+		.tested		= TEST_UNTESTED,
+		.probe		= probe_spi_rdid,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 256} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 32} },
+				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 16} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {1024 * 1024, 1} },
+				.block_erase = spi_block_erase_60,
+			}, {
+				.eraseblocks = { {1024 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		/* TODO: security register */
+		.printlock	= spi_prettyprint_status_register_bp3_srwd, /* bit6 is quad enable */
+		.unlock		= spi_disable_blockprotect_bp3_srwd,
+		.write		= spi_chip_write_256,
+		.read		= spi_chip_read, /* Fast read (0x0B) and multi I/O supported */
+		.voltage	= {2300, 3600},
+	},
+
+
 	{
 		.vendor		= "Macronix",
 		.name		= "MX29F001B",
