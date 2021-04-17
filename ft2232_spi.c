236c4ab6f8f355d1ced5ad601a6c517168fc728d
Angel Pons <th3fanbus@gmail.com>
2021-04-17 17:07:34 +0200

    ft2232_spi.c: Fix typo in comment
    his ---> this
    
    Change-Id: Ibcc04a1581b3ba0dcd86e6f900c146823ebcd84a
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/52471
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>

--- ft2232_spi.c
+++ ft2232_spi.c
@@ -27,7 +27,7 @@
 #include <ftdi.h>
 
 /* This is not defined in libftdi.h <0.20 (c7e4c09e68cfa6f5e112334aa1b3bb23401c8dc7 to be exact).
- * Some tests indicate that his is the only change that it is needed to support the FT232H in flashrom. */
+ * Some tests indicate that this is the only change that it is needed to support the FT232H in flashrom. */
 #if !defined(HAVE_FT232H)
 #define TYPE_232H	6
 #endif
