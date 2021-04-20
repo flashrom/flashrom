47aff8c2f11cabbd899f24f4e627c1b556bd698c
Christian Kudera <coreboot@kudera.at>
2021-04-20 22:50:01 +0200

    flashchips: Add support for Boya/BoHong Microelectronics B_25D16A
    Read tested on CH341A
    
    Change-Id: I25b776204affda94cc7e753e7671ef9d3d9508f1
    Signed-off-by: Christian Kudera <coreboot@kudera.at>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/52555
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- flashchips.c
+++ flashchips.c
@@ -3482,6 +3482,44 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "Boya/BoHong Microelectronics",
+		.name		= "B.25D16A",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= BOYA_BOHONG_ID,
+		.model_id	= BOYA_BOHONG_B_25D16A,
+		.total_size	= 2048,
+		.page_size	= 256,
+		.feature_bits	= FEATURE_WRSR_WREN,
+		.tested		= TEST_OK_PR,
+		.probe		= probe_spi_rdid,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 512} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 64} },
+				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 32} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {2 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_60,
+			}, {
+				.eraseblocks = { {2 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.printlock	= spi_prettyprint_status_register_bp2_srwd,
+		.unlock		= spi_disable_blockprotect_bp2_srwd,
+		.write		= spi_chip_write_256,
+		.read		= spi_chip_read,
+		.voltage	= {2700, 3600},
+	},
+
+	{
+		.vendor		= "Boya/BoHong Microelectronics",
 		.name		= "B.25Q128AS",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= BOYA_BOHONG_ID,
--- flashchips.h
+++ flashchips.h
@@ -210,6 +210,7 @@
 
 /* Boya/BoHong Microelectronics Inc. */
 #define BOYA_BOHONG_ID		0x68
+#define BOYA_BOHONG_B_25D16A	0x4015
 #define BOYA_BOHONG_B_25Q128AS	0x4018
 
 /* Bright Microelectronics has the same manufacturer ID as Hyundai... */
