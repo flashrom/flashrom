c1173784781d81c5d601a65fbfc61d550d50f377
Angel Pons <th3fanbus@gmail.com>
2021-05-02 15:09:20 +0200

    nicintel_eeprom.c: Mark 8086:1531 as tested
    Reading, erasing and writing works on an i210 NIC of an Asus Z10PA-D8.
    
    Change-Id: I9cabea5dfb9424b9c30d82840089506f2bd943da
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/52825
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>

--- nicintel_eeprom.c
+++ nicintel_eeprom.c
@@ -88,7 +88,7 @@ const struct dev_entry nics_intel_ee[] = {
 	{PCI_VENDOR_ID_INTEL, 0x1511, NT , "Intel", "82580 Quad Gigabit Ethernet Controller (Ext. PHY)"},
 	{PCI_VENDOR_ID_INTEL, 0x1511, NT , "Intel", "82580 Dual Gigabit Ethernet Controller (Copper)"},
 	{PCI_VENDOR_ID_INTEL, UNPROG_DEVICE, OK, "Intel", "Unprogrammed 82580 Quad/Dual Gigabit Ethernet Controller"},
-	{PCI_VENDOR_ID_INTEL, 0x1531, NT, "Intel", "I210 Gigabit Network Connection Unprogrammed"},
+	{PCI_VENDOR_ID_INTEL, 0x1531, OK, "Intel", "I210 Gigabit Network Connection Unprogrammed"},
 	{PCI_VENDOR_ID_INTEL, 0x1532, NT, "Intel", "I211 Gigabit Network Connection Unprogrammed"},
 	{PCI_VENDOR_ID_INTEL, 0x1533, OK, "Intel", "I210 Gigabit Network Connection"},
 	{PCI_VENDOR_ID_INTEL, 0x1536, NT, "Intel", "I210 Gigabit Network Connection SERDES Fiber"},
