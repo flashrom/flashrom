flashrom/flashrom

commit e3707bbf497d4d845477f1b84ece51e8ab117ffc

dummyflasher.c: Replace atoi() with strtoul()
BUG=none
BRANCH=none
TEST=builds

Change-Id: Ib9e66016a2f4ce2d13b833c261f900cab80916b7
Spotted-by: Angel Pons <th3fanbus@gmail.com>
Signed-off-by: Edward O'Callaghan <quasisec@google.com>
Reviewed-on: https://review.coreboot.org/c/flashrom/+/54854
Reviewed-by: Sam McNally <sammc@google.com>
Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

dummyflasher.c
@@ -668,6 +668,7 @@ int dummy_init(void)
 #if EMULATE_CHIP
 	struct stat image_stat;
 #endif
+	char *endptr;
 
 	struct emu_data *data = calloc(1, sizeof(struct emu_data));
 	if (!data) {
@@ -710,9 +711,9 @@ int dummy_init(void)
 
 	tmp = extract_programmer_param("spi_write_256_chunksize");
 	if (tmp) {
-		data->spi_write_256_chunksize = atoi(tmp);
+		data->spi_write_256_chunksize = strtoul(tmp, &endptr, 0);
 		free(tmp);
-		if (data->spi_write_256_chunksize < 1) {
+		if (*endptr != '\0' || data->spi_write_256_chunksize < 1) {
 			msg_perr("invalid spi_write_256_chunksize\n");
 			return 1;
 		}
@@ -977,7 +978,6 @@ int dummy_init(void)
 #ifdef EMULATE_SPI_CHIP
 	status = extract_programmer_param("spi_status");
 	if (status) {
-		char *endptr;
 		errno = 0;
 		data->emu_status = strtoul(status, &endptr, 0);
 		free(status);

commit 51c1f39e915e24707e5a0632d03816d1e19fae85

dummyflasher.c: Fix use-after-free issue
Issue accidentally introduced in commit e3707bbf4.

BUG=none
BRANCH=none
TEST=builds

Change-Id: Ie12d6474ef5a7bb1afbbb9ec50b31b577da1a81b
Signed-off-by: Edward O'Callaghan <quasisec@google.com>
Reviewed-on: https://review.coreboot.org/c/flashrom/+/55014
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Angel Pons <th3fanbus@gmail.com>
Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>

dummyflasher.c
@@ -700,12 +700,13 @@ int dummy_init(void)
 	tmp = extract_programmer_param("spi_write_256_chunksize");
 	if (tmp) {
 		data->spi_write_256_chunksize = strtoul(tmp, &endptr, 0);
-		free(tmp);
 		if (*endptr != '\0' || data->spi_write_256_chunksize < 1) {
 			msg_perr("invalid spi_write_256_chunksize\n");
+			free(tmp);
 			return 1;
 		}
 	}
+	free(tmp);
 
 	tmp = extract_programmer_param("spi_blacklist");
 	if (tmp) {