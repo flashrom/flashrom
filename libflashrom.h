72fd4f194e8988bc0dcf99d2929408b23b39bf75
Thomas Heijligen <thomas.heijligen@secunet.de>
2021-06-09 11:56:54 +0200

    libflashrom: remove flashrom_system_info
    void flashrom_system_info(void) has no implementation nor any relation
    to existing code.
    
    Change-Id: Iad805322aeead149a8d51ad8bf0babde2a12f01f
    Signed-off-by: Thomas Heijligen <thomas.heijligen@secunet.de>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/55351
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- libflashrom.h
+++ libflashrom.h
@@ -75,7 +75,6 @@ struct flashrom_chipset_info {
 };
 
 const char *flashrom_version_info(void);
-void flashrom_system_info(void);
 struct flashrom_flashchip_info *flashrom_supported_flash_chips(void);
 struct flashrom_board_info *flashrom_supported_boards(void);
 struct flashrom_chipset_info *flashrom_supported_chipsets(void);
