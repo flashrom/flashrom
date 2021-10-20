89b11595f95d332cedb92af5cedbf61ec12dbd39
Simon Buhrow <simon.buhrow@sieb-meyer.de>
2021-10-20 17:09:09 +0200

    flashchips.c: mark IS25LP064 as TEST_OK_PREW
    Tested '-w', '-E' and '-r' successfully with
    my FT2232H programmer.
    
    Change-Id: I2197ce0be9db7c3d74b24c7445dc06238584ffea
    Signed-off-by: Simon Buhrow <simon.buhrow@posteo.de>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/58472
    Reviewed-by: Felix Singer <felixsinger@posteo.net>
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- flashchips.c
+++ flashchips.c
@@ -7387,7 +7387,7 @@ const struct flashchip flashchips[] = {
 		.page_size	= 256,
 		/* OTP: 1024B total; read 0x48; write 0x42 */
 		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP,
-		.tested		= TEST_UNTESTED,
+		.tested		= TEST_OK_PREW,
 		.probe		= probe_spi_rdid,
 		.probe_timing	= TIMING_ZERO,
 		.block_erasers	=
