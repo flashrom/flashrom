55aa056c7404e0912ed2e56b682512df7a0cca77
Edward O'Callaghan <quasisec@google.com>
2021-12-13 12:46:12 +1100

    flashrom.c: Move do_*() helpers into cli_classic.c
    These helpers are only used by the CLI logic and so we localise
    them here to move towards cli_classic being a pure libflashrom
    user.
    
    BUG=b:208132085
    TEST=`make`
    
    Change-Id: If1112155e2421e0178fd73f847cbb80868387433
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/60070
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>

--- flash.h
+++ flash.h
@@ -356,10 +356,6 @@ int write_buf_to_file(const unsigned char *buf, unsigned long size, const char *
 int write_buf_to_include_args(const struct flashctx *const flash, unsigned char *buf);
 int prepare_flash_access(struct flashctx *, bool read_it, bool write_it, bool erase_it, bool verify_it);
 void finalize_flash_access(struct flashctx *);
-int do_read(struct flashctx *, const char *filename);
-int do_extract(struct flashctx *);
-int do_write(struct flashctx *, const char *const filename, const char *const referencefile);
-int do_verify(struct flashctx *, const char *const filename);
 int register_chip_restore(chip_restore_fn_cb_t func, struct flashctx *flash, uint8_t status);
 
 /* Something happened that shouldn't happen, but we can go on. */
