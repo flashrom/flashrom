0c3bdd9fc6cf98f7599b5aa7bfd64ca975b946ee
Thomas Heijligen <thomas.heijligen@secunet.com>
2022-01-06 14:49:31 +0100

    nicintel_eeprom: remove unused rget_io_perms()
    The nicintel_eeprom programmer does not use x86 IO Ports. Remove the
    dependency to it.
    
    Change-Id: I5fd42572fd29f5d7fd749c2836eac3e68c947946
    Signed-off-by: Thomas Heijligen <thomas.heijligen@secunet.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/60845
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Felix Singer <felixsinger@posteo.net>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- nicintel_eeprom.c
+++ nicintel_eeprom.c
@@ -35,7 +35,6 @@
 #include "spi.h"
 #include "programmer.h"
 #include "hwaccess.h"
-#include "hwaccess_x86_io.h"
 #include "hwaccess_physmap.h"
 #include "platform/pci.h"
 
@@ -462,9 +461,6 @@ static const struct opaque_master opaque_master_nicintel_ee_i210 = {
 
 static int nicintel_ee_init(void)
 {
-	if (rget_io_perms())
-		return 1;
-
 	struct pci_dev *dev = pcidev_init(nics_intel_ee, PCI_BASE_ADDRESS_0);
 	if (!dev)
 		return 1;
