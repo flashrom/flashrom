c829a48e198d723f0e6ae1ff6d6f820eb085711d
aarya <aarya.chaumal@gmail.com>
2022-03-10 09:16:44 +0530

    serprog.c: Avoid calling memcpy with NULL pointer arguments
    In function sp_stream_buffer_op, the variable parms might be NULL when
    passed to memcpy. Although, since parmlen is also 0 at that time I
    don't think it would make much difference. Still, add a NULL check
    before calling memcpy to be safe.
    
    Change-Id: I850123237e328f9548ba7f77a01888be2cbc9e7b
    Signed-off-by: Aarya Chaumal <aarya.chaumal@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/62726
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Reviewed-by: Thomas Heijligen <src@posteo.de>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- serprog.c
+++ serprog.c
@@ -291,7 +291,8 @@ static int sp_stream_buffer_op(uint8_t cmd, uint32_t parmlen, uint8_t *parms)
 		return 1;
 	}
 	sp[0] = cmd;
-	memcpy(&(sp[1]), parms, parmlen);
+	if (parms)
+		memcpy(&(sp[1]), parms, parmlen);
 
 	if (sp_streamed_transmit_bytes >= (1 + parmlen + sp_device_serbuf_size)) {
 		if (sp_flush_stream() != 0) {
