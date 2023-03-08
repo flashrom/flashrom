5b8b6995a7d39c22c726b92324471a7c399a3135
PoroCYon <p@pcy.be>
2023-03-08 22:33:07 +0100

    flashchips: Add Macronix MX25V4035F support
    See https://www.macronix.com/Lists/Datasheet/Attachments/8670/MX25V4035F,%202.5V,%204Mb,%20v1.2.pdf .
    
    I've only tested this patch with the MX25V1635F I have here, though
    other chips in the series exist as well. Tested using serprog and ftdi
    by writing a few images to the flash and seeing if changes were stored
    correctly.
    
    Change-Id: I8b26926c354b840ca7b14b4c5cb000e3c02f5137
    Signed-off-by: PoroCYon <p@pcy.be>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/73582
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>

--- flashchips.c
+++ flashchips.c
@@ -9576,6 +9576,42 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "Macronix",
+		.name		= "MX25V4035F",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= MACRONIX_ID,
+		.model_id	= MACRONIX_MX25V4035F,
+		.total_size	= 512,
+		.page_size	= 256,
+		/* OTP: 8KiB total; enter 0xB1, exit 0xC1 */
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_QPI | FEATURE_SCUR,
+		.tested		= TEST_UNTESTED,
+		.probe		= PROBE_SPI_RDID,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { { 4 * 1024, 128} },
+				.block_erase = SPI_BLOCK_ERASE_20,
+			}, {
+				.eraseblocks = { {32 * 1024,  16} },
+				.block_erase = SPI_BLOCK_ERASE_52,
+			}, {
+				.eraseblocks = { {64 * 1024,   8} },
+				.block_erase = SPI_BLOCK_ERASE_D8,
+			}, {
+				.eraseblocks = { {512 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_C7,
+			}
+		},
+		.printlock	= SPI_PRETTYPRINT_STATUS_REGISTER_BP3_SRWD, /* bit6 is quad enable */
+		.unlock		= SPI_DISABLE_BLOCKPROTECT_BP3_SRWD,
+		.write		= SPI_CHIP_WRITE256, /* Multi I/O supported */
+		.read		= SPI_CHIP_READ, /* Fast read (0x0B) and multi I/O supported */
+		.voltage	= {2300, 3600},
+	},
+
+	{
+		.vendor		= "Macronix",
 		.name		= "MX25U12835F",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= MACRONIX_ID,
