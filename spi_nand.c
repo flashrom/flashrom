e6ec56d9dfd441d748235c8e2f5c6ca68ef25805
froloffw7 <69122526+froloffw7@users.noreply.github.com>
2023-05-25 22:58:10 +0200

    Update spi_nand.c
    Copyright

--- spi_nand.c
+++ spi_nand.c
@@ -2,6 +2,7 @@
  * This file is part of the flashrom project.
  *
  * Copyright (C) 2022 Yangfl
+ * Copyright (C) 2023 froloff
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
