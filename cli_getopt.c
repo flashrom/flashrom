690ec44e0bbd624bf5cb2a7205c0273847207526
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2024-11-09 11:44:22 +0100

    cli: Use SPDX in headers
    Change-Id: Ie523cf5b26a29de8e9e9efb56efe04a5ff86a59c
    Signed-off-by: Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/85073
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- cli_getopt.c
+++ cli_getopt.c
@@ -1,28 +1,10 @@
 /*
  * This file is part of the flashrom project.
- * It comes originally from the musl libc project and is licensed under the
- * terms of the MIT license.
+ * It comes originally from the musl libc project.
  *
- * Copyringht (C) 2023 Rich Felker and the musl authors
- * Adjusted for flashrom by Thomas Heijligen<thomas.heijligen@secunet.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2023 Rich Felker and the musl authors
+ * SPDX-FileCopyrightText: Thomas Heijligen <thomas.heijligen@secunet.com>
  */
 
 #include <cli_getopt.h>
