4c6df1e168e96f90a9768c7f27224adb1fa29015
Matt DeVillier <matt.devillier@gmail.com>
2025-02-20 15:44:31 -0600

    flashchips/winbond: Update test status for Winbond W25Q128.W
    Tested probe, read, erase, write, and WP operations on a Winbond
    W25Q128.W chip using internal and ch341a_spi programmers.
    
    Change-Id: Ia1f2a5f4942a4f1956405afa5b56c9e38101f2be
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/86544
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- flashchips/winbond.c
+++ flashchips/winbond.c
@@ -216,7 +216,7 @@
 		/* supports SFDP */
 		/* OTP: 1024B total, 256B reserved; read 0x48; write 0x42, erase 0x44, read ID 0x4B */
 		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_QPI | FEATURE_WRSR2,
-		.tested		= TEST_OK_PREW,
+		.tested		= TEST_OK_PREWB,
 		.probe		= PROBE_SPI_RDID,
 		.probe_timing	= TIMING_ZERO,
 		.block_erasers	=
