c0bf7928a2c11c1645ee69b75a6ca48e2b92f47d
Andranux <andranux+coding@posteo.de>
2025-07-06 12:33:02 +0200

    flashchips: Add EN25QX128A
    I tested with an "ch341a" usb adapter.
    I was able to read, erase and write successfully.
    
    Datasheet: https://www.esmt.com.tw/upload/pdf/ESMT/datasheets/EN25QX128A(2V).pdf
    
    Change-Id: If6c5c3c37f3d817d93abdbc60c2d9280ff2585c3
    Signed-off-by: Andranux <andranux+coding@posteo.de>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/88327
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- flashchips/eon.c
+++ flashchips/eon.c
@@ -1580,6 +1580,47 @@
 
 	{
 		.vendor		= "Eon",
+		.name		= "EN25QX128A",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= EON_ID_NOPREFIX,
+		.model_id	= EON_EN25QX128A,
+		.total_size	= 16384,
+		.page_size	= 256,
+		/* supports SFDP */
+		/* OTP: 512B total;*/
+		/* QPI enable 0x38, disable 0xFF */
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_QPI,
+		.tested		= TEST_OK_PREW,
+		.probe		= PROBE_SPI_RDID,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 4096} },
+				.block_erase = SPI_BLOCK_ERASE_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 512} },
+				.block_erase = SPI_BLOCK_ERASE_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 256} },
+				.block_erase = SPI_BLOCK_ERASE_D8,
+			}, {
+				.eraseblocks = { {16 * 1024 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_60,
+			}, {
+				.eraseblocks = { {16 * 1024 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_C7,
+			}
+		},
+		.printlock	= SPI_PRETTYPRINT_STATUS_REGISTER_BP3_SRWD, /* bit6 is quad enable */
+		.unlock		= SPI_DISABLE_BLOCKPROTECT_BP3_SRWD,
+		.write		= SPI_CHIP_WRITE256,
+		.read		= SPI_CHIP_READ,
+		.voltage	= {2700, 3600},
+	},
+
+	{
+		.vendor		= "Eon",
 		.name		= "EN25S10",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= EON_ID_NOPREFIX,
