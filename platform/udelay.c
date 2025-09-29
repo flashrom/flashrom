3471cba18a1a9d15a19e0407dacee7e081afd0b6
Antonio Vázquez <antoniovazquezblanco@gmail.com>
2025-09-29 10:35:53 +0200

    platform: Use SPDX in headers
    Change-Id: I3322366ebfa7db1d1f1e0fe60c808efd839b2858
    Signed-off-by: Antonio Vázquez <antoniovazquezblanco@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/89369
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- platform/udelay.c
+++ platform/udelay.c
@@ -1,19 +1,10 @@
 /*
  * This file is part of the flashrom project.
  *
- * Copyright (C) 2000 Silicon Integrated System Corporation
- * Copyright (C) 2009,2010 Carl-Daniel Hailfinger
- * Copyright (C) 2024 Google LLC
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-FileCopyrightText: 2000 Silicon Integrated System Corporation
+ * SPDX-FileCopyrightText: 2009,2010 Carl-Daniel Hailfinger
+ * SPDX-FileCopyrightText: 2024 Google LLC
  */
 
 #ifndef __LIBPAYLOAD__
