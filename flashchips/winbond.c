2fdf7ef60558d59041b8d6bcdf2833df0ec4ec15
Sultan Alsawaf <sultan@kerneltoast.com>
2025-11-30 16:24:29 -0800

    flashchips: add Winbond W25R256JW
    The W25R256JW flash chip is a 32 MiB chip similar to W25Q256JW, with the
    major feature difference being that W25R256JW comes with RPMC
    (Replay-Protected Monotonic Counter). While W25R256JW has the same
    device ID (0x6019) as W25Q256JW, it cannot use the W25Q256JW definition
    because it doesn't support as many block erasure instructions.
    
    W25R256JW has three status registers, all with the same bit layout as
    W25Q256JW's three status registers.
    
    Probing, erasing, reading, and writing have been successfully tested on
    an HP 16-ag1000 laptop (HP OmniBook 5 Laptop Next Gen AI 16-ag1070wm)
    which uses W25R256JW. The tested programmer was ch347_spi with
    spispeed=7.5M because the default of 15 MHz didn't work (probe failed).
    W25R256JW supports up to 104 MHz so the failure with 15 MHz is probably
    just specific to probing the chip when it's soldered on this specific
    laptop's mainboard.
    
    Speeds are observed to be good for all tested operations.
    
    Datasheet: https://www.winbond.com/resource-files/W25R256JW%20RevC%2006062022.pdf
    
    Change-Id: Id63521ca12ed595f3eb0abb892ed4529cda60425
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/90298
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- flashchips/winbond.c
+++ flashchips/winbond.c
@@ -751,6 +751,57 @@
 
 	{
 		.vendor		= "Winbond",
+		.name		= "W25R256JW",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= WINBOND_NEX_ID,
+		.model_id	= WINBOND_NEX_W25Q256_W,
+		.total_size	= 32768,
+		.page_size	= 256,
+		/* supports SFDP */
+		/* OTP: 3X256B; read 0x48; write 0x42, erase 0x44, read ID 0x4B */
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_4BA | FEATURE_WRSR2
+				  | FEATURE_WRSR3,
+		.tested		= TEST_OK_PREW,
+		.probe		= PROBE_SPI_RDID,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 8192} },
+				.block_erase = SPI_BLOCK_ERASE_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 1024} },
+				.block_erase = SPI_BLOCK_ERASE_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 512} },
+				.block_erase = SPI_BLOCK_ERASE_D8,
+			}, {
+				.eraseblocks = { {32 * 1024 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_60,
+			}, {
+				.eraseblocks = { {32 * 1024 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_C7,
+			}
+		},
+		.printlock	= SPI_PRETTYPRINT_STATUS_REGISTER_BP3_SRWD,
+		.unlock		= SPI_DISABLE_BLOCKPROTECT,
+		.write		= SPI_CHIP_WRITE256,
+		.read		= SPI_CHIP_READ,
+		.voltage	= {1700, 1950},
+		.reg_bits	=
+		{
+			.srp	= {STATUS1, 7, RW},
+			.srl	= {STATUS2, 0, RW},
+			.bp	= {{STATUS1, 2, RW}, {STATUS1, 3, RW}, {STATUS1, 4, RW}, {STATUS1, 5, RW}},
+			.tb	= {STATUS1, 6, RW},
+			.cmp	= {STATUS2, 6, RW},
+			.wps	= {STATUS3, 2, RW},
+		},
+		.decode_range	= DECODE_RANGE_SPI25,
+	},
+
+	{
+		.vendor		= "Winbond",
 		.name		= "W25R512NW/W74M51NW",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= WINBOND_NEX_ID,
