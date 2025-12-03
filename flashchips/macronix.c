fc80103b2bb234cb97c8278dc77aa41d47d3cc6d
whitebkues1 <whitebkues1@gmail.com>
2025-12-03 12:36:43 +0800

    flashchips: Add support for MXIC MX66U2G45G
    The MX66U2G45G has been tested by ch341a programmer : read, write, erase
    
    MX66U2G45G datasheet is available at the following URL:
    https://www.mxic.com.tw/Lists/Datasheet/Attachments/8743/MX66U2G45G,%201.8V,%202Gb,%20v1.1.pdf
    
    Change-Id: I20fde29468a203060df01e6bfb188e65006a861f
    Signed-off-by: Xian Zheng  <whitebkues1@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/90110
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>

--- flashchips/macronix.c
+++ flashchips/macronix.c
@@ -2959,6 +2959,55 @@
 
 	{
 		.vendor		= "Macronix",
+		.name		= "MX66U2G45G",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= MACRONIX_ID,
+		.model_id	= MACRONIX_MX66U2G45G,
+		.total_size	= 256 * 1024,
+		.page_size	= 256,
+		/* OTP: 512B total; enter 0xB1, exit 0xC1 */
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_4BA,
+		.tested		= TEST_OK_PREW,
+		.probe		= PROBE_SPI_RDID,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 65536} },
+				.block_erase = SPI_BLOCK_ERASE_21,
+			}, {
+				.eraseblocks = { {4 * 1024, 65536} },
+				.block_erase = SPI_BLOCK_ERASE_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 8192} },
+				.block_erase = SPI_BLOCK_ERASE_5C,
+			}, {
+				.eraseblocks = { {32 * 1024, 8192} },
+				.block_erase = SPI_BLOCK_ERASE_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 4096} },
+				.block_erase = SPI_BLOCK_ERASE_DC,
+			}, {
+				.eraseblocks = { {64 * 1024, 4096} },
+				.block_erase = SPI_BLOCK_ERASE_D8,
+			}, {
+				.eraseblocks = { {256 * 1024 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_60,
+			}, {
+				.eraseblocks = { {256 * 1024 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_C7,
+			}
+		},
+		/* TODO: security register and SBLK/SBULK, configuration register */
+		.printlock	= SPI_PRETTYPRINT_STATUS_REGISTER_BP3_SRWD, /* bit6 is quad enable */
+		.unlock		= SPI_DISABLE_BLOCKPROTECT_BP3_SRWD,
+		.write		= SPI_CHIP_WRITE256,
+		.read		= SPI_CHIP_READ, /* Fast read (0x0B) supported */
+		.voltage	= {1650, 2000},
+	},
+
+	{
+		.vendor		= "Macronix",
 		.name		= "MX77L25650F",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= MACRONIX_ID,
