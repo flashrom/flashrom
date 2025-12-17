b4577cfa76c41f7b2e405968354102dbe1babaf8
Jae Hyun Yoo <jae.yoo@oss.qualcomm.com>
2025-12-16 23:48:30 -0800

    flashchip: add support for W25Q01JV
    Add support for Winbond W25Q01JV [0].
    
    Tested: probe, read, write and erase [1].
    Programmer: Raspberry Pi 5 with SPI interface enabled.
    
    [0]: https://www.winbond.com/hq/product/code-storage-flash-memory/serial-nor-flash/?__locale=en&partNo=W25Q01JV
    [1]: https://paste.flashrom.org/view.php?id=3798
    
    Change-Id: Iac251e47969f16551803d5fede474f29d8120dcb
    Signed-off-by: Jae Hyun Yoo <jae.yoo@oss.qualcomm.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/90543
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- flashchips/winbond.c
+++ flashchips/winbond.c
@@ -1478,6 +1478,50 @@
 	},
 
 	{
+		.vendor		= "Winbond",
+		.name		= "W25Q01JV",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= WINBOND_NEX_ID,
+		.model_id	= WINBOND_NEX_W25Q01JV,
+		.total_size	= 128 * 1024,
+		.page_size	= 256,
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_4BA,
+		.tested		= TEST_OK_PREW,
+		.probe		= PROBE_SPI_RDID,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 32768} },
+				.block_erase = SPI_BLOCK_ERASE_21,
+			}, {
+				.eraseblocks = { {4 * 1024, 32768} },
+				.block_erase = SPI_BLOCK_ERASE_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 4096} },
+				.block_erase = SPI_BLOCK_ERASE_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 2048} },
+				.block_erase = SPI_BLOCK_ERASE_DC,
+			}, {
+				.eraseblocks = { {64 * 1024, 2048} },
+				.block_erase = SPI_BLOCK_ERASE_D8,
+			}, {
+				.eraseblocks = { {128 * 1024 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_60,
+			}, {
+				.eraseblocks = { {128 * 1024 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_C7,
+			}
+		},
+		.printlock	= SPI_PRETTYPRINT_STATUS_REGISTER_PLAIN,
+		.unlock		= SPI_DISABLE_BLOCKPROTECT,
+		.write		= SPI_CHIP_WRITE256,
+		.read		= SPI_CHIP_READ,
+		.voltage	= {2700, 3600},
+	},
+
+	{
 		.vendor         = "Winbond",
 		.name           = "W25Q512NW-IM",
 		.bustype        = BUS_SPI,
