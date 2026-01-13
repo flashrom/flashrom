655f1e6d543e56f9140a79eaed07d718e5e1279f
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-01-13 19:42:43 +0100

    platform/udelay: Document file and functions
    Change-Id: I13b514a68c01e1cf8882bdc29a0bb18309722b2d
    Signed-off-by: Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/90746
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- include/platform/udelay.h
+++ include/platform/udelay.h
@@ -3,12 +3,34 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  * SPDX-FileCopyrightText: 2025 Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
+ *
+ * This header provides declarations for delay functions used to implement
+ * microsecond-level timing delays in a platform-independent manner.
  */
 
 #ifndef __UDELAY_H__
 #define __UDELAY_H__
 
+/**
+ * @brief Delay for at least the specified number of microseconds, with low precision.
+ * @details
+ * This function will always us an OS-provided sleep()-like function which will usually
+ * save power but may delay for significantly longer than intended, especially if
+ * the delay is very short. Callers should prefer to use default_delay().
+ * @param usecs The number of microseconds to delay.
+ */
 void internal_sleep(unsigned int usecs);
+
+/**
+ * @brief Delay for at least the specified number of microseconds.
+ * @details
+ * For very short delays this function polls the wall time provided by the OS,
+ * otherwise it will use an OS-provided sleep()-like function. The delay will never
+ * be shorter than the precision of an OS clock source (which usually have nanosecond
+ * precision), but will usually be closer to the requested time than an equivalent
+ * delay using internal_sleep().
+ * @param usecs The number of microseconds to delay.
+ */
 void default_delay(unsigned int usecs);
 
 #endif
