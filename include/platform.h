66eea7af5dd0f52383724bae2948b17a1a6bc72d
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-01-21 11:18:47 +0100

    include/platform.h: Add documentation
    Change-Id: I0093227d807b0cb81cc03640016859a2e3d0abab
    Signed-off-by: Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/flashrom/+/90846
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- include/platform.h
+++ include/platform.h
@@ -4,10 +4,10 @@
  * SPDX-License-Identifier: GPL-2.0-only
  * SPDX-FileCopyrightText: 2009 Carl-Daniel Hailfinger
  * SPDX-FileCopyrightText: 2022 secunet Security Networks AG (written by Thomas Heijligen <thomas.heijligen@secunet.com)
- */
-
-/*
- * Header file for platform abstraction.
+ *
+ * This header file provides functions for endianness conversions between CPU native endianness and little/big endian
+ * formats, as well as functions to read values from memory with specified endianness.
+ * These utilities abstract different byte orders across the supported hardware platforms.
  */
 
 #ifndef __PLATFORM_H__
@@ -16,40 +16,182 @@
 #include <stddef.h>
 #include <stdint.h>
 
-/* convert cpu native endian to little endian */
+/**
+ * \brief Converts an 8-bit unsigned integer from CPU native endianness to little endian.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint8_t  cpu_to_le8 (uint8_t  value);
+
+/**
+ * \brief Converts a 16-bit unsigned integer from CPU native endianness to little endian.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint16_t cpu_to_le16(uint16_t value);
+
+/**
+ * \brief Converts a 32-bit unsigned integer from CPU native endianness to little endian.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint32_t cpu_to_le32(uint32_t value);
+
+/**
+ * \brief Converts a 64-bit unsigned integer from CPU native endianness to little endian.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint64_t cpu_to_le64(uint64_t value);
 
-/* convert cpu native endian to big endian */
+/**
+ * \brief Converts an 8-bit unsigned integer from CPU native endianness to big endian.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint8_t  cpu_to_be8 (uint8_t  value);
+
+/**
+ * \brief Converts a 16-bit unsigned integer from CPU native endianness to big endian.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint16_t cpu_to_be16(uint16_t value);
+
+/**
+ * \brief Converts a 32-bit unsigned integer from CPU native endianness to big endian.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint32_t cpu_to_be32(uint32_t value);
+
+/**
+ * \brief Converts a 64-bit unsigned integer from CPU native endianness to big endian.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint64_t cpu_to_be64(uint64_t value);
 
-/* convert little endian to cpu native endian */
+/**
+ * \brief Converts an 8-bit unsigned integer from little endian to CPU native endianness.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint8_t  le_to_cpu8 (uint8_t  value);
+
+/**
+ * \brief Converts a 16-bit unsigned integer from little endian to CPU native endianness.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint16_t le_to_cpu16(uint16_t value);
+
+/**
+ * \brief Converts a 32-bit unsigned integer from little endian to CPU native endianness.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint32_t le_to_cpu32(uint32_t value);
+
+/**
+ * \brief Converts a 64-bit unsigned integer from little endian to CPU native endianness.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint64_t le_to_cpu64(uint64_t value);
 
-/* convert big endian to cpu native endian */
+
+/**
+ * \brief Converts an 8-bit unsigned integer from big endian to CPU native endianness.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint8_t  be_to_cpu8 (uint8_t  value);
+
+/**
+ * \brief Converts a 16-bit unsigned integer from big endian to CPU native endianness.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint16_t be_to_cpu16(uint16_t value);
+
+/**
+ * \brief Converts a 32-bit unsigned integer from big endian to CPU native endianness.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint32_t be_to_cpu32(uint32_t value);
+
+/**
+ * \brief Converts a 64-bit unsigned integer from big endian to CPU native endianness.
+ * \param value The value to convert.
+ * \return The converted value.
+ */
 uint64_t be_to_cpu64(uint64_t value);
 
-/* read value from base at offset in little endian */
+/**
+ * \brief Reads an 8-bit unsigned integer from memory in little endian format.
+ * \param base The base address to read from.
+ * \param offset The offset from the base address.
+ * \return The read value in CPU native endianness.
+ */
 uint8_t  read_le8 (const void *base, size_t offset);
+
+/**
+ * \brief Reads a 16-bit unsigned integer from memory in little endian format.
+ * \param base The base address to read from.
+ * \param offset The offset from the base address.
+ * \return The read value in CPU native endianness.
+ */
 uint16_t read_le16(const void *base, size_t offset);
+
+/**
+ * \brief Reads a 32-bit unsigned integer from memory in little endian format.
+ * \param base The base address to read from.
+ * \param offset The offset from the base address.
+ * \return The read value in CPU native endianness.
+ */
 uint32_t read_le32(const void *base, size_t offset);
+
+/**
+ * \brief Reads a 64-bit unsigned integer from memory in little endian format.
+ * \param base The base address to read from.
+ * \param offset The offset from the base address.
+ * \return The read value in CPU native endianness.
+ */
 uint64_t read_le64(const void *base, size_t offset);
 
-/* read value from base at offset in big endian */
+
+/**
+ * \brief Reads an 8-bit unsigned integer from memory in big endian format.
+ * \param base The base address to read from.
+ * \param offset The offset from the base address.
+ * \return The read value in CPU native endianness.
+ */
 uint8_t  read_be8 (const void *base, size_t offset);
+
+/**
+ * \brief Reads a 16-bit unsigned integer from memory in big endian format.
+ * \param base The base address to read from.
+ * \param offset The offset from the base address.
+ * \return The read value in CPU native endianness.
+ */
 uint16_t read_be16(const void *base, size_t offset);
+
+/**
+ * \brief Reads a 32-bit unsigned integer from memory in big endian format.
+ * \param base The base address to read from.
+ * \param offset The offset from the base address.
+ * \return The read value in CPU native endianness.
+ */
 uint32_t read_be32(const void *base, size_t offset);
+
+/**
+ * \brief Reads a 64-bit unsigned integer from memory in big endian format.
+ * \param base The base address to read from.
+ * \param offset The offset from the base address.
+ * \return The read value in CPU native endianness.
+ */
 uint64_t read_be64(const void *base, size_t offset);
 
 #endif /* !__PLATFORM_H__ */
